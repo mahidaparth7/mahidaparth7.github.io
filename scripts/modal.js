eec10d28731e560a220ef2df352088d3597b2e4a
parth.mahida <parth@upmetrics.co>
2017-10-30 17:37:38 +0530

    colored icons

--- scripts/modal.js
+++ scripts/modal.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       detail: 'Sunwardflag is an ecommerce website that sales USA Flags.',
       link: 'http://www.roambi.com'
     },
-    nodeCMS: {
+    nodecms: {
       title: 'NodeCMS',
       tag: 'CMS',
       detail: 'CMS based on wordpress structure developed with node and angular.',
